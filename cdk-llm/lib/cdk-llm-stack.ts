44271c483a2179518fd2b613e5ecc7cb7d442028
ksdyb.park <ksdyb@amazon.com>
2023-07-10 18:07:44 +0900

    update

--- cdk-llm/lib/cdk-llm-stack.ts
+++ cdk-llm/lib/cdk-llm-stack.ts
@@ -4,19 +4,66 @@ import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
 import * as logs from "aws-cdk-lib/aws-logs"
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
+
+const debug = false;
+const stage = 'dev';
 
 export class CdkLlmStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // s3 
+    const s3Bucket = new s3.Bucket(this, "chatbot-storage",{
+      // bucketName: bucketName,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      versioned: false,
+    });
+    if(debug) {
+      new cdk.CfnOutput(this, 'bucketName', {
+        value: s3Bucket.bucketName,
+        description: 'The nmae of bucket',
+      });
+      new cdk.CfnOutput(this, 's3Arn', {
+        value: s3Bucket.bucketArn,
+        description: 'The arn of s3',
+      });
+      new cdk.CfnOutput(this, 's3Path', {
+        value: 's3://'+s3Bucket.bucketName,
+        description: 'The path of s3',
+      });
+    }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLlmQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-    // Lambda for stable diffusion 
+    // copy web application files into s3 bucket
+    new s3Deploy.BucketDeployment(this, "upload-HTML", {
+      sources: [s3Deploy.Source.asset("../html")],
+      destinationBucket: s3Bucket,
+    });
+
+    // cloudfront
+    const distribution = new cloudFront.Distribution(this, 'cloudfront', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(s3Bucket),
+        allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+        viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      priceClass: cloudFront.PriceClass.PRICE_CLASS_200,  
+    });
+    new cdk.CfnOutput(this, 'distributionDomainName', {
+      value: distribution.domainName,
+      description: 'The domain name of the Distribution',
+    });
+
+    // Lambda for chat
     const lambdaChatApi = new lambda.Function(this, 'lambda-chat', {
       description: 'lambda for chat api',
       functionName: 'lambda-chat-api',
@@ -32,6 +79,89 @@ export class CdkLlmStack extends cdk.Stack {
         // domain: cloudFrontDomain,
         // nproc: String(1)
       }
+    });
+
+    const SageMakerPolicy = new iam.PolicyStatement({  // policy statement for sagemaker
+      actions: ['sagemaker:*'],
+      resources: ['*'],
+    });
+    lambdaChatApi.role?.attachInlinePolicy( // add sagemaker policy
+      new iam.Policy(this, 'sagemaker-policy', {
+        statements: [SageMakerPolicy],
+      }),
+    );
+    lambdaChatApi.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));  // permission for api Gateway
+
+    // role
+    const role = new iam.Role(this, "api-role-chatbot", {
+      roleName: "api-role-chatbot",
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com")
+    });
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: ['lambda:InvokeFunction']
+    }));
+    role.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+    }); 
+
+    // API Gateway
+    const api = new apiGateway.RestApi(this, 'api-chatbot', {
+      description: 'API Gateway for chatbot',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+      deployOptions: {
+        stageName: stage,
+
+        // logging for debug
+        // loggingLevel: apiGateway.MethodLoggingLevel.INFO, 
+        // dataTraceEnabled: true,
+      },
+    });  
+
+    // POST method
+    const chat = api.root.addResource('chat');
+    chat.addMethod('POST', new apiGateway.LambdaIntegration(lambdaChatApi, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [   // API Gateway sends to the client that called a method.
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+
+    new cdk.CfnOutput(this, 'apiUrl-chat', {
+      value: api.url,
+      description: 'The url of API Gateway',
+    }); 
+    new cdk.CfnOutput(this, 'curlUrl-chat', {
+      value: "curl -X POST "+api.url+'chat -H "Content-Type: application/json" -d \'{"text":"who are u?"}\'',
+      description: 'Curl commend of API Gateway',
+    }); 
+
+    // cloudfront setting for api gateway of stable diffusion
+    distribution.addBehavior("/chat", new origins.RestApiOrigin(api), {
+      cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+      allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,  
+      viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });    
+    
+    new cdk.CfnOutput(this, 'WebUrl', {
+      value: 'https://'+distribution.domainName+'/chat.html',      
+      description: 'The web url of request for chat',
+    });
+
+    new cdk.CfnOutput(this, 'UpdateCommend', {
+      value: 'aws s3 cp ../html/chat.js '+'s3://'+s3Bucket.bucketName,
+      description: 'The url of web file upload',
     });
   }
 }
--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -8,82 +8,73 @@ from multiprocessing import Process
 
 s3 = boto3.client('s3')
 
-def parse_response(query_response):    
-    response_dict = json.loads(query_response)
-    return response_dict["generated_images"], response_dict["prompt"]
-
-def stable_diffusion(num, txt, width, height, mybucket, fname, endpoint):
-    mykey = fname+'_'+str(num)+'.jpeg'  
-    start = int(time.time())
-
-    print("endpoint: ", endpoint)
-
-    payload = {        
-        "prompt": txt,
-        "width": width,
-        "height": height,
-        "num_images_per_prompt": 1,
-        "num_inference_steps": 50,
-        "guidance_scale": 7.5,
-    }
-
-    runtime = boto3.Session().client('sagemaker-runtime')
-    response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Accept='application/json;jpeg', Body=json.dumps(payload))
-    
-    statusCode = response['ResponseMetadata']['HTTPStatusCode']
-    print('statusCode:', json.dumps(statusCode))
-    
-    if(statusCode==200):
-        response_payload = response['Body'].read().decode('utf-8')
-        generated_images, prompt = parse_response(response_payload)
-
-        #print(response_payload)
-        #print(generated_images[0])
-        print(prompt)
-        
-        img_str = base64.b64decode(generated_images[0])
-        buffer = io.BytesIO(img_str) 
-
-        s3.upload_fileobj(buffer, mybucket, mykey, ExtraArgs={"ContentType": "image/jpeg"})
-        
-        print("---> run time(sec): ", int(time.time()) - start)
-
-
 def query_endpoint(payload, endpoint_name):
-        newline, bold, unbold = '\n', '\033[1m', '\033[0m'
-
         client = boto3.client('runtime.sagemaker')
-        response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=json.dumps(payload).encode('utf-8'))
-        model_predictions = json.loads(response['Body'].read())
-        generated_text = model_predictions[0]['generated_text']
-        print (
-            f"Input Text: {payload['inputs']}{newline}"
-            f"Generated Text: {bold}{generated_text}{unbold}{newline}")
+        response = client.invoke_endpoint(
+             EndpointName=endpoint_name, 
+             ContentType='application/json', 
+             Body=json.dumps(payload).encode('utf-8'))                
+        print('response:', response)
+        
+        statusCode = response['ResponseMetadata']['HTTPStatusCode']        
+        if(statusCode==200):
+            response_payload = json.loads(response['Body'].read())
+            print('response_payload:', response_payload)
+
+            outputText = ""
+            print('len:', len(response_payload))
+            if len(response_payload) == 1:
+                outputText = response_payload[0]['generated_text']
+            else:
+                for resp in response_payload:
+                    outputText = resp['generated_text'] + '\n'
+                
+            return {
+                 'statusCode': statusCode,
+                 'body': outputText
+            }
+            
+        else:
+            return {
+                 'statusCode': statusCode,
+                 'body': json.dumps(response)
+            }
     
 def lambda_handler(event, context):
     print(event)
 
+    text = event['text']
+
     start = int(time.time())
 
     payload = {
-    "inputs": "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
-    "parameters":{
-        "max_new_tokens": 50,
-        "return_full_text": False,
-        "do_sample": True,
-        "top_k":10
+        #"inputs": "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
+        "inputs": text,
+        "parameters":{
+            "max_new_tokens": 50,
+            "return_full_text": False,
+            "do_sample": True,
+            "top_k":10
         }
     }
     
     endpoint_name = 'jumpstart-dft-hf-llm-falcon-7b-instruct-bf16'
 
-    query_endpoint(payload, endpoint_name)
+    response = query_endpoint(payload, endpoint_name)
+
+    generated_text = response['body']
+    statusCode = response['statusCode']
+
+    newline, bold, unbold = '\n', '\033[1m', '\033[0m'
+    print (
+        f"Input Text: {payload['inputs']}{newline}"
+        f"Generated Text: {bold}{generated_text}{unbold}{newline}")
 
     elapsed_time = int(time.time()) - start
     print("total run time(sec): ", elapsed_time)
 
-    statusCode = 200     
     return {
         'statusCode': statusCode,
-        # 'body': json.dumps(urls),
+        'msg': generated_text,
     }
+
