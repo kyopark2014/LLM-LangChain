a9940818e352d78843a30897f89e7199abd61766
ksdyb.park <ksdyb@amazon.com>
2023-07-15 20:33:40 +0900

    update

--- cdk-llm-lambda/lambda-summary/lambda_function.py
+++ cdk-llm-lambda/lambda-summary/lambda_function.py
@@ -44,7 +44,7 @@ llm = SagemakerEndpoint(
     content_handler = content_handler
 )
 
-def get_summary_from_pdf(file_type, s3_file_name):
+def get_summary(file_type, s3_file_name):
     summary = ''
     
     if file_type == 'pdf':
@@ -81,24 +81,67 @@ def get_summary_from_pdf(file_type, s3_file_name):
         chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
         summary = chain.run(docs)
         print('summary: ', summary)
+
+    elif file_type == 'txt':        
+        s3r = boto3.resource("s3")
+        doc = s3r.Object(s3_bucket, s3_prefix+'/'+s3_file_name)
+
+        from langchain.document_loaders import S3FileLoader
+        loader = S3FileLoader(s3_bucket, s3_prefix+'/'+s3_file_name)
+        text = loader.load()    
+        print(text)
+
+        contents = doc.get()['Body'].read()
+        reader = str(BytesIO(contents))
+        
+        raw_text = []
+        for page in reader.pages:
+            raw_text.append(page.extract_text())
+        contents = '\n'.join(raw_text)    
+
+        new_contents = str(contents).replace("\n"," ") 
+        print('length: ', len(new_contents))
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
+        texts = text_splitter.split_text(new_contents) 
+        print('texts[0]: ', texts[0])
+        
+        docs = [
+            Document(
+                page_content=t
+            ) for t in texts[:3]
+        ]
+        prompt_template = """Write a concise summary of the following:
+
+        {text}
+        
+        CONCISE SUMMARY """
+
+        PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+        chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
+        summary = chain.run(docs)
+        print('summary: ', summary)
             
     return summary    
-        
+
 def lambda_handler(event, context):
     print(event)
 
     object = event['object']
     print('object: ', object)
 
-    print('.: ',object.rfind('.'))
-    #var ext = url_file.substring(url_file.lastIndexOf('.') + 1).toLowerCase();
-    ext = object[object.rfind('.')+1:len(object)]
-    print('ext: ', ext)
+    file_type = object[object.rfind('.')+1:len(object)]
+    print('file_type: ', file_type)
     
     start = int(time.time())
     
-    summary = get_summary_from_pdf('pdf', object)
-
+    if file_type == 'pdf':
+        summary = get_summary('pdf', object)
+    elif file_type == 'txt':
+        summary = get_summary('txt', object)
+    else:
+        summary = ''
+        
     elapsed_time = int(time.time()) - start
     print("total run time(sec): ", elapsed_time)
 
