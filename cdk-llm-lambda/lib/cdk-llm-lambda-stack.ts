439e7d4775436d1ec8b984c90a20db1585694ed4
ksdyb.park <ksdyb@amazon.com>
2023-07-16 00:28:07 +0900

    update

--- cdk-llm-lambda/lib/cdk-llm-lambda-stack.ts
+++ cdk-llm-lambda/lib/cdk-llm-lambda-stack.ts
@@ -131,7 +131,7 @@ export class CdkLlmLambdaStack extends cdk.Stack {
     const api = new apiGateway.RestApi(this, 'api-chatbot-llm', {
       description: 'API Gateway for chatbot',
       endpointTypes: [apiGateway.EndpointType.REGIONAL],
-      binaryMediaTypes: ['application/pdf', 'text/plain'], 
+      binaryMediaTypes: ['application/pdf', 'text/plain', 'text/csv'], 
       deployOptions: {
         stageName: stage,
 
