180026dbac666c5e741384baaeff1bb7bca9b477
ksdyb.park <ksdyb@amazon.com>
2023-07-15 21:29:33 +0900

    update

--- cdk-llm-lambda/lambda-summary/lambda_function.py
+++ cdk-llm-lambda/lambda-summary/lambda_function.py
@@ -93,7 +93,7 @@ def get_summary(file_type, s3_file_name):
         print(text)
         """
 
-        cont = doc.get()['Body'].read().decode('utf-8')
+        cont = doc.get()['Body'].read().decode()
         print('cont: ', cont)
 
         contents = str(BytesIO(cont))
