b2caee7a4342ef2c049a51232ebe23565f0e46ca
ksdyb.park <ksdyb@amazon.com>
2023-07-16 09:50:11 +0900

    update

--- cdk-llm-lambda/lib/cdk-llm-lambda-stack.ts
+++ cdk-llm-lambda/lib/cdk-llm-lambda-stack.ts
@@ -238,7 +238,7 @@ export class CdkLlmLambdaStack extends cdk.Stack {
       integrationResponses: [{
         statusCode: '200',
       }], 
-      proxy:true, 
+      proxy:false, 
     }), {
       methodResponses: [  
         {
